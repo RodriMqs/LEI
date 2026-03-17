8f0059ec513e874adf801f4cd2093febc1ea1bbd
Rodriguinhozinho <rodrigomarques.rm862@gmail.com>
2026-03-17 14:53:13 +0000

    ex1

--- 1-Ano/2-Semestre/AED/ListasLigadas/OutrasOperacoesGrupoA.h
+++ 1-Ano/2-Semestre/AED/ListasLigadas/OutrasOperacoesGrupoA.h
@@ -14,7 +14,20 @@ PNodoLista criarListaAleatoria (int);
 
 PNodoLista criarListaAleatoria (int N)
 {
+    PNodoLista L = criarLista();
+    DadosLista X;
+    int N, int i = 0;
+    while (i < N)
+    {
+        X = criarElementoLista();
+        if(pesquisarLista(X, L) == NULL)
+        {
+            L = inserirListaCabeca(X, L);
+            i++;
+        }
 
+    }
+    return L;
 }
 
 
