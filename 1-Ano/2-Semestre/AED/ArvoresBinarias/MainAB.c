46c7a9cd8dac97d8abb264462b9f84ee4e8e34e7
Rodriguinhozinho <rodrigomarques.rm862@gmail.com>
2026-04-21 15:27:10 +0100

    Arvores Binarias

--- 1-Ano/2-Semestre/AED/ArvoresBinarias/MainAB.c
+++ 1-Ano/2-Semestre/AED/ArvoresBinarias/MainAB.c
@@ -1,7 +1,6 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <limits.h>
 #include "Aleatorio.h"
 #include "OperacoesBasicasAB.h"
 #include "EADArvoreBinaria.h"
@@ -10,6 +9,23 @@
 
 int main()
 {
-
+    PNodoAB T;
+    int TAM = 5;
+    //int TAM = gerarNumeroInteiro(0,15);
+    T = criarABAleatoria(TAM);
+    printf("Arvore Binaria Aleatoria:\n");
+    printf("Em ordem:\n");
+    mostrarEmOrdemAB(T);
+    /**printf("Pre ordem:\n");
+    mostrarPreOrdemAB(T);
+    printf("Pos ordem:\n");
+    mostrarPosOrdemAB(T);**/
+    printf("Por Níveis:\n");
+    mostrarPorNiveisAB(T);
+    int totk = contaKAB(T , 10);
+    printf("Total de 10s = %d\n", totk);
+    //ex 6
+    int maior = procurarMaior(T);
+    printf("Maior elemento = %d:\n" , maior);
 
 }
--- 1-Ano/2-Semestre/AED/ArvoresBinarias/OutrasOperacoesAB.h
+++ 1-Ano/2-Semestre/AED/ArvoresBinarias/OutrasOperacoesAB.h
@@ -4,15 +4,56 @@
 /* ------------------------------------------------------- */
 
 PNodoAB criarABAleatoria (int);
-
-
-
+int contarNodosRecursivaAB(PNodoAB T);
+int contaKAB (PNodoAB T, int K);
+int procurarMaior(PNodoAB T);
 /* ------------------------------------------------------- */
 /* -------------- implementação das funções -------------- */
 /* ------------------------------------------------------- */
 
 PNodoAB criarABAleatoria (int N)
 {
+    PNodoAB T = criarAB();
+    int i=0;
+    DadosAB X;
+    while(i<N){
+        X=criarElementoAB();
+        if(pesquisarAB(X,T)==NULL){
+            T=inserirPorAlturaAB(X,T);
+           i++;
+        }
+    }
+    return T;
+
+}
+//ex 3
+int contarNodosRecursivaAB (PNodoAB T)
+{
+    if(T==NULL)
+        return 0;
+    return 1 + contarNodosRecursivaAB(T->Esquerda) + contarNodosRecursivaAB(T->Direita);
+}
+//ex 4
+int contaKAB (PNodoAB T, int K){
+    if(T==NULL)
+        return 0;
+    if(T->Elemento.notaFinal == K)
+        return 1 + contaKAB(T->Esquerda, K) + contaKAB(T->Direita, K);
+    return contaKAB(T->Esquerda, K) + contaKAB(T->Direita, K);
+}
 
 
+//ex 6
+int procurarMaior(PNodoAB T)
+{
+    if(T==NULL)
+        return INT_MIN;
+    int maior_esquerda = procurarMaior(T->Esquerda);
+    int maior_direita = procurarMaior(T->Direita);
+    int maior = T->Elemento.notaFinal;
+    if (maior_esquerda > maior)
+        maior = maior_esquerda;
+    if (maior_direita > maior)
+        maior = maior_direita;
+    return maior;
 }
